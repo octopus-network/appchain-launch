7e636c1c798ff4c63b8684a8b0a486c83b183c75
ruanyu <>
2021-09-23 17:32:03 +0800

    octopus-score-counter

---
+++ octopus-score-counter/main.tf
@@ -0,0 +1,164 @@
+provider "google" {
+  project = var.project
+  region  = var.region
+}
+
+data "google_client_config" "default" {
+}
+
+data "google_container_cluster" "default" {
+  name     = var.cluster
+  location = var.region
+}
+
+provider "kubernetes" {
+  host                   = "https://${data.google_container_cluster.default.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(data.google_container_cluster.default.master_auth[0].cluster_ca_certificate)
+}
+
+# 
+# data "kubernetes_namespace" "default" {
+#   metadata {
+#     name = "default"
+#   }
+# }
+
+resource "kubernetes_namespace" "default" {
+  metadata {
+    labels = {
+      name = "octopus-score-counter"
+    }
+    name = "octopus-score-counter"
+  }
+}
+
+resource "kubernetes_service_account" "default" {
+  metadata {
+    name        = "score-counter-ksa"
+    namespace   = kubernetes_namespace.default.metadata.0.name
+    annotations = {
+      "iam.gke.io/gcp-service-account" = var.service_account
+    }
+  }
+}
+
+data "google_service_account" "default" {
+  account_id = var.service_account
+}
+
+resource "google_service_account_iam_member" "default" {
+  service_account_id = data.google_service_account.default.name
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "serviceAccount:${var.project}.svc.id.goog[${kubernetes_namespace.default.metadata.0.name}/${kubernetes_service_account.default.metadata.0.name}]"
+}
+
+resource "kubernetes_secret" "default" {
+  metadata {
+    name      = "score-counter-secret"
+    namespace = kubernetes_namespace.default.metadata.0.name
+  }
+  data = {
+    PGUSER     = var.database.username
+    PGPASSWORD = var.database.password
+    PGDATABASE = var.database.database
+
+    ADMIN_PRIVATE_KEY = var.contract.private_key
+  }
+}
+
+resource "kubernetes_config_map" "default" {
+  metadata {
+    name      = "score-counter-config-map"
+    namespace = kubernetes_namespace.default.metadata.0.name
+  }
+  data = {
+    GCE_PROXY_INSTANCES = var.gce_proxy_instances
+    PGHOST              = var.database.host
+    PGPORT              = var.database.port
+
+    NEAR_NODE_URL   = var.near.node_url
+    NEAR_WALLET_URL = var.near.wallet_url
+    NEAR_HELPER_URL = var.near.helper_url
+
+    NETWORK_ID                   = var.contract.network_id
+    REGISTRY_CONTRACT_ID         = var.contract.contract_id
+    ADMIN_ACCOUNT_ID             = var.contract.account_id
+    COUNTING_INTERVAL_IN_SECONDS = var.contract.counting_interval
+  }
+}
+
+resource "kubernetes_deployment" "default" {
+  metadata {
+    name = "score-counter"
+    labels = {
+      app = "score-counter"
+    }
+    namespace = kubernetes_namespace.default.metadata.0.name
+  }
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        app = "score-counter"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "score-counter"
+        }
+      }
+      spec {
+        container {
+          name    = "cloud-sql-proxy"
+          image   = var.gce_proxy_image
+          command = ["/cloud_sql_proxy", "-instances=$(GCE_PROXY_INSTANCES)"]
+          env {
+            name = "GCE_PROXY_INSTANCES"
+            value_from {
+              config_map_key_ref {
+                name = kubernetes_config_map.default.metadata.0.name
+                key  = "GCE_PROXY_INSTANCES"
+              }
+            }
+          }
+          resources {
+            requests = {
+              cpu    = "1000m"
+              memory = "2048Mi"
+            }
+          }
+          security_context {
+            run_as_non_root = true
+          }
+        }
+        container {
+          name  = "score-counter"
+          image = var.score_counter_image
+          env_from {
+            secret_ref {
+              name = kubernetes_secret.default.metadata.0.name
+            }
+          }
+          env_from {
+            config_map_ref {
+              name = kubernetes_config_map.default.metadata.0.name
+            }
+          }
+          resources {
+            requests = {
+              cpu    = "200m"
+              memory = "256Mi"
+            }
+            limits = {
+              cpu    = "1000m"
+              memory = "1024Mi"
+            }
+          }
+        }
+        service_account_name = kubernetes_service_account.default.metadata.0.name
+      }
+    }
+  }
+}
---
+++ octopus-score-counter/variables.tf
@@ -0,0 +1,70 @@
+variable "score_counter_image" {
+  description = "Octopus Score Counter Image"
+  type        = string
+}
+
+# postgresql
+variable "database" {
+  description = "DB Configuration"
+  type = object({
+    username = string
+    password = string
+    database = string
+    host     = string
+    port     = string
+  })
+}
+
+variable "gce_proxy_image" {
+  description = "GCE Proxy Image"
+  type        = string
+}
+
+variable "gce_proxy_instances" {
+  description = "GCE Proxy Instances"
+  type        = string
+}
+
+variable "service_account" {
+  description = "Google Service Account"
+  type        = string
+}
+
+# near
+variable "near" {
+  description = "Near Configuration"
+  type = object({
+    node_url   = string
+    wallet_url = string
+    helper_url = string
+  })
+}
+
+# contract
+variable "contract" {
+  description = "Contract Configuration"
+  type = object({
+    network_id        = string
+    contract_id       = string
+    account_id        = string
+    private_key       = string
+    counting_interval = number
+  })
+  sensitive = true
+}
+
+# gke
+variable "project" {
+  description = "Project"
+  type        = string
+}
+
+variable "region" {
+  description = "Region"
+  type        = string
+}
+
+variable "cluster" {
+  description = "Cluster"
+  type        = string
+}
