32309b21ee1e578a5e52aa1833b599dfd5b9bb1a
ruanyu <ruanyu.gemini@gmail.com>
2023-09-09 12:41:16 +0800

    Add --overwrite option to hermes keys add command

--- hermes/init.sh
+++ hermes/init.sh
@@ -7,5 +7,5 @@ CHAIN_ID_2=$3
 CREDENTIAL_2=$4
 
 # TODO: /home/hermes/.hermes/keys/near-0
-hermes keys add --chain $CHAIN_ID_1 --key-file $CREDENTIAL_1
-hermes keys add --chain $CHAIN_ID_2 --key-file $CREDENTIAL_2
+hermes keys add --overwrite --chain $CHAIN_ID_1 --key-file $CREDENTIAL_1
+hermes keys add --overwrite --chain $CHAIN_ID_2 --key-file $CREDENTIAL_2
