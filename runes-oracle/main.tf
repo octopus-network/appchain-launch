50f2622d138b636d2f70e92392aa8bb4b8b5bf38
ruanyu <ruanyu.gemini@gmail.com>
2024-04-14 17:25:00 +0800

    Add runes-oracle

---
+++ runes-oracle/main.tf
@@ -0,0 +1,88 @@
+provider "google" {
+  project = var.project
+  region  = var.region
+}
+
+data "google_client_config" "default" {
+}
+
+data "google_container_cluster" "default" {
+  name     = var.cluster
+  location = var.region
+}
+
+provider "kubernetes" {
+  host                   = "https://${data.google_container_cluster.default.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(data.google_container_cluster.default.master_auth[0].cluster_ca_certificate)
+}
+
+data "kubernetes_namespace" "default" {
+  metadata {
+    name = var.namespace
+  }
+}
+
+resource "kubernetes_config_map" "default" {
+  metadata {
+    name      = "runes-oracle-config-map"
+    namespace = data.kubernetes_namespace.default.metadata.0.name
+  }
+  data = {
+    INDEXER_URL         = var.INDEXER_URL
+    IC_GATEWAY          = var.IC_GATEWAY
+    CUSTOMS_CANISTER_ID = var.CUSTOMS_CANISTER_ID
+  }
+}
+
+resource "kubernetes_deployment" "default" {
+  metadata {
+    name = "runes-oracle"
+    labels = {
+      app = "runes-oracle"
+    }
+    namespace = data.kubernetes_namespace.default.metadata.0.name
+  }
+  spec {
+    replicas = var.runes_oracle.replicas
+    selector {
+      match_labels = {
+        app = "runes-oracle"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "runes-oracle"
+        }
+      }
+      spec {
+        container {
+          name    = "runes-oracle"
+          image   = var.runes_oracle.image
+          command = ["runes_oracle"]
+          env_from {
+            config_map_ref {
+              name = kubernetes_config_map.default.metadata.0.name
+            }
+          }
+          resources {
+            limits = {
+              cpu    = var.runes_oracle.resources.cpu_limits
+              memory = var.runes_oracle.resources.memory_limits
+            }
+            requests = {
+              cpu    = var.runes_oracle.resources.cpu_requests
+              memory = var.runes_oracle.resources.memory_requests
+            }
+          }
+        }
+      }
+    }
+  }
+  lifecycle {
+    ignore_changes = [
+      spec[0].template[0].spec[0].container[0].resources
+    ]
+  }
+}
---
+++ runes-oracle/variables.tf
@@ -0,0 +1,53 @@
+# gke
+variable "project" {
+  description = "Project"
+  type        = string
+}
+
+variable "region" {
+  description = "Region"
+  type        = string
+}
+
+variable "cluster" {
+  description = "Cluster"
+  type        = string
+}
+
+variable "namespace" {
+  description = "Namespace"
+  type        = string
+  default     = "default" # devnet / testnet / mainnet
+}
+
+# pod
+variable "runes_oracle" {
+  description = "Runes Oracle Configuration"
+  type = object({
+    image    = string
+    replicas = number
+
+    resources = object({
+      cpu_requests    = string
+      cpu_limits      = string
+      memory_requests = string
+      memory_limits   = string
+    })
+  })
+}
+
+# env variables
+variable "INDEXER_URL" {
+  description = "INDEXER_URL"
+  type        = string
+}
+
+variable "IC_GATEWAY" {
+  description = "IC_GATEWAY"
+  type        = string
+}
+
+variable "CUSTOMS_CANISTER_ID" {
+  description = "CUSTOMS_CANISTER_ID"
+  type        = string
+}
