4435aac9a15efd20b27d3624c08747128edf0680
research_deallinker <ruanyu@deallinker.com>
2024-05-19 18:44:32 +0800

    Update octopus-triggers

--- octopus-triggers/main.tf
+++ octopus-triggers/main.tf
@@ -24,62 +24,15 @@ data "kubernetes_namespace" "default" {
   }
 }
 
-resource "kubernetes_service_account" "default" {
-  metadata {
-    name        = "triggers-ksa"
-    namespace   = data.kubernetes_namespace.default.metadata.0.name
-    annotations = {
-      "iam.gke.io/gcp-service-account" = var.service_account
-    }
-  }
-}
-
-data "google_service_account" "default" {
-  account_id = var.service_account
-}
-
-resource "google_service_account_iam_member" "default" {
-  service_account_id = data.google_service_account.default.name
-  role               = "roles/iam.workloadIdentityUser"
-  member             = "serviceAccount:${var.project}.svc.id.goog[${data.kubernetes_namespace.default.metadata.0.name}/${kubernetes_service_account.default.metadata.0.name}]"
-}
-
-resource "kubernetes_secret" "default" {
-  metadata {
-    name      = "triggers-secret"
-    namespace = data.kubernetes_namespace.default.metadata.0.name
-  }
-  data = {
-    PGUSER     = var.database.username
-    PGPASSWORD = var.database.password
-    PGDATABASE = var.database.database
-
-    ADMIN_PRIVATE_KEY = var.contract.private_key
-  }
-}
-
 resource "kubernetes_config_map" "default" {
   metadata {
     name      = "triggers-config-map"
     namespace = data.kubernetes_namespace.default.metadata.0.name
   }
   data = {
-    LISTENING_PORT = var.triggers.listening_port
-
-    GCE_PROXY_INSTANCES = var.gce_proxy_instances
-    PGHOST              = var.database.host
-    PGPORT              = var.database.port
-
-    NEAR_NODE_URL   = var.near.node_url
-    NEAR_WALLET_URL = var.near.wallet_url
-    NEAR_HELPER_URL = var.near.helper_url
-
-    NETWORK_ID                   = var.contract.network_id
-    REGISTRY_CONTRACT_ID         = var.contract.contract_id
-    TOKEN_CONTRACT_ID            = var.contract.token_contract_id
-    ADMIN_ACCOUNT_ID             = var.contract.account_id
-    COUNTING_INTERVAL_IN_SECONDS = var.contract.counting_interval
-    DAO_CONTRACT_ID              = var.contract.dao_contract_id
+    BTC_ENV                     = var.BTC_ENV
+    BTC_CUSTOMS_DEPOSIT_ADDRESS = var.BTC_CUSTOMS_DEPOSIT_ADDRESS
+    CANISTERS                   = var.CANISTERS
   }
 }
 
@@ -106,37 +59,8 @@ resource "kubernetes_deployment" "default" {
       }
       spec {
         container {
-          name    = "cloud-sql-proxy"
-          image   = var.gce_proxy_image
-          command = ["/cloud_sql_proxy", "-instances=$(GCE_PROXY_INSTANCES)"]
-          env {
-            name = "GCE_PROXY_INSTANCES"
-            value_from {
-              config_map_key_ref {
-                name = kubernetes_config_map.default.metadata.0.name
-                key  = "GCE_PROXY_INSTANCES"
-              }
-            }
-          }
-          resources {
-            requests = {
-              cpu    = var.gce_proxy_resources.cpu_requests
-              memory = var.gce_proxy_resources.memory_requests
-            }
-          }
-          security_context {
-            run_as_non_root = true
-          }
-        }
-        container {
-          name  = "triggers-app"
+          name  = "triggers"
           image = var.triggers.image
-          command = var.triggers.app_cmd == null ? null : split(" ", var.triggers.app_cmd)
-          env_from {
-            secret_ref {
-              name = kubernetes_secret.default.metadata.0.name
-            }
-          }
           env_from {
             config_map_ref {
               name = kubernetes_config_map.default.metadata.0.name
@@ -144,143 +68,17 @@ resource "kubernetes_deployment" "default" {
           }
           resources {
             requests = {
-              cpu    = var.triggers_resources.cpu_requests
-              memory = var.triggers_resources.memory_requests
+              cpu    = var.triggers.resources.cpu_requests
+              memory = var.triggers.resources.memory_requests
             }
           }
         }
-        container {
-          name  = "triggers-server"
-          image = var.triggers.image
-          command = var.triggers.server_cmd == null ? null : split(" ", var.triggers.server_cmd)
-          env_from {
-            secret_ref {
-              name = kubernetes_secret.default.metadata.0.name
-            }
-          }
-          env_from {
-            config_map_ref {
-              name = kubernetes_config_map.default.metadata.0.name
-            }
-          }
-          resources {
-            requests = {
-              cpu    = var.triggers_resources.cpu_requests
-              memory = var.triggers_resources.memory_requests
-            }
-          }
-        }
-        service_account_name = kubernetes_service_account.default.metadata.0.name
       }
     }
   }
   lifecycle {
     ignore_changes = [
       spec[0].template[0].spec[0].container[0].resources,
-      spec[0].template[0].spec[0].container[1].resources
     ]
-  }
-}
-
-resource "kubernetes_manifest" "default" {
-  manifest = {
-    apiVersion = "cloud.google.com/v1"
-    kind       = "BackendConfig"
-    metadata   = {
-      name      = "triggers-backendconfig"
-      namespace = data.kubernetes_namespace.default.metadata.0.name
-    }
-    spec = {
-      healthCheck = {
-        type        = "HTTP"
-        requestPath = "/healthz"
-        port        = var.triggers.listening_port
-      }
-    }
-  }
-}
-
-resource "kubernetes_service" "default" {
-  metadata {
-    name      = "triggers"
-    namespace = data.kubernetes_namespace.default.metadata.0.name
-    labels = {
-      app  = "triggers"
-    }
-    annotations = {
-      "cloud.google.com/neg" = "{\"ingress\": true}"
-      "cloud.google.com/backend-config" = "{\"default\": \"triggers-backendconfig\"}"
-    }
-  }
-  spec {
-    type = "NodePort"
-    selector = {
-      app  = "triggers"
-    }
-    port {
-      port        = var.triggers.listening_port
-      target_port = var.triggers.listening_port
-      protocol    = "TCP"
-    }
-  }
-}
-
-resource "google_compute_global_address" "default" {
-  name = "triggers-global-address"
-}
-
-data "google_dns_managed_zone" "default" {
-  name = var.dns_zone
-}
-
-resource "google_dns_record_set" "a" {
-  name         = "triggers.${data.google_dns_managed_zone.default.dns_name}"
-  managed_zone = data.google_dns_managed_zone.default.name
-  type         = "A"
-  ttl          = 300
-  rrdatas = [google_compute_global_address.default.address]
-}
-
-resource "google_dns_record_set" "caa" {
-  name         = "triggers.${data.google_dns_managed_zone.default.dns_name}"
-  managed_zone = data.google_dns_managed_zone.default.name
-  type         = "CAA"
-  ttl          = 300
-  rrdatas = ["0 issue \"pki.goog\""]
-}
-
-resource "kubernetes_manifest" "certificate" {
-  manifest = {
-    apiVersion = "networking.gke.io/v1"
-    kind       = "ManagedCertificate"
-    metadata   = {
-      name      = "triggers-managed-certificate"
-      namespace = data.kubernetes_namespace.default.metadata.0.name
-    }
-    spec = {
-      domains = [trimsuffix(google_dns_record_set.a.name, ".")]
-    }
-  }
-}
-
-resource "kubernetes_ingress_v1" "default" {
-  metadata {
-    name        = "triggers-ingress"
-    namespace   = data.kubernetes_namespace.default.metadata.0.name
-    annotations = {
-      "kubernetes.io/ingress.global-static-ip-name" = google_compute_global_address.default.name
-      "networking.gke.io/managed-certificates"      = "triggers-managed-certificate"
-      "kubernetes.io/ingress.class"                 = "gce"
-    }
-  }
-  spec {
-    default_backend {
-      service {
-        name = kubernetes_service.default.metadata.0.name
-        port {
-          number = var.triggers.listening_port
-        }
-      }
-    }
   }
 }
--- octopus-triggers/variables.tf
+++ octopus-triggers/variables.tf
@@ -1,89 +1,3 @@
-variable "triggers" {
-  description = "Octopus Triggers Configuration"
-  type = object({
-    image          = string
-    app_cmd        = optional(string)
-    server_cmd     = optional(string)
-    listening_port = string
-  })
-}
-
-variable "triggers_resources" {
-  description = "Octopus Score Counter Resources"
-  type        = object({
-    cpu_requests    = string
-    memory_requests = string
-  })
-  default = {
-    cpu_requests    = "150m"
-    memory_requests = "256Mi"
-  }
-}
-
-# postgresql
-variable "database" {
-  description = "DB Configuration"
-  type = object({
-    username = string
-    password = string
-    database = string
-    host     = string
-    port     = string
-  })
-}
-
-variable "gce_proxy_image" {
-  description = "GCE Proxy Image"
-  type        = string
-}
-
-variable "gce_proxy_instances" {
-  description = "GCE Proxy Instances"
-  type        = string
-}
-
-variable "gce_proxy_resources" {
-  description = "Octopus GCE Proxy Resources"
-  type        = object({
-    cpu_requests      = string
-    memory_requests   = string
-  })
-  default = {
-    cpu_requests    = "100m"
-    memory_requests = "256Mi"
-  }
-}
-
-variable "service_account" {
-  description = "Google Service Account"
-  type        = string
-}
-
-# near
-variable "near" {
-  description = "Near Configuration"
-  type = object({
-    node_url   = string
-    wallet_url = string
-    helper_url = string
-  })
-}
-
-# contract
-variable "contract" {
-  description = "Contract Configuration"
-  type = object({
-    network_id        = string
-    contract_id       = string
-    token_contract_id = string
-    account_id        = string
-    private_key       = string
-    counting_interval = number
-    dao_contract_id   = string
-  })
-  sensitive = true
-}
-
 # gke
 variable "project" {
   description = "Project"
@@ -106,7 +20,31 @@ variable "namespace" {
   default     = "default" # devnet / testnet / mainnet
 }
 
-variable "dns_zone" {
-  description = "DNS Zone"
+# octopus-triggers
+variable "triggers" {
+  description = "Octopus Triggers Configuration"
+  type = object({
+    image = string
+    resources = object({
+      cpu_requests    = string
+      cpu_limits      = string
+      memory_requests = string
+      memory_limits   = string
+    })
+  })
+}
+
+variable "BTC_ENV" {
+  description = "BTC_ENV"
+  type        = string
+}
+
+variable "BTC_CUSTOMS_DEPOSIT_ADDRESS" {
+  description = "BTC_CUSTOMS_DEPOSIT_ADDRESS"
+  type        = string
+}
+
+variable "CANISTERS" {
+  description = "CANISTERS"
   type        = string
 }
